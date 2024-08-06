8db00be6b94c5ec64a09de7f10e2f90ab4e1ee86
Michael Chi <kalschi@google.com>
2024-08-06 06:50:48 +0000

    remove installation_scrit -> should be another PR

--- installation_scripts/terraform/GWSsecretManager/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-#  backend "gcs" {
-#    bucket  = "csm_automation"
-#    prefix  = "terraform/state/secretmanager/"
-#  }
-  backend "local" {
-    path = "./terraform.tfstate"
-  }
-}
--- installation_scripts/terraform/cloudSql/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-#  backend "gcs" {
-#    bucket  = "csm_automation"
-#    prefix  = "terraform/state/cloudSql"
-#  }
-  backend "local" {
-    path = "./terraform.tfstate"
-  }
-}
